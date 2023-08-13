e3b33afffa0679ac7a24c90917091af3390b010b
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2023-08-13 14:33:09 -0300

    Criacao do metodo responsavel por adicionar os valores digitados pelo usuario

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ function updateDisplay() {
         display.textContent = secondValue;
     }
 }
+
+function appendToDisplay(character) {
+    if (selectedOperation === "") {
+        if (firstValue === "0" && character !== ".") {
+            firstValue = character;
+        } else {
+            firstValue += character;
+        }
+        updateDisplay();
+    } else {
+        if (secondValue === "0" && character !== ".") {
+            secondValue = character;
+        } else {
+            secondValue += character;
+        }
+        updateDisplay();
+    }
+}
